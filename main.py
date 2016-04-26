c0daea8e50899bdbac8c217579d07ecef86d96ba
ark-1 <arkadiy.bazhanov@mail.ru>
2016-04-26 22:14:28 +0300

    Generating list and writing it to an HTML page

--- main.py
+++ main.py
@@ -11,10 +11,11 @@ def get_time(string):
 MIN_COMMENTS = 15                           # Number of comments user need to have to get into our list.
 SUNSET = get_time('T' + '00:00:00' + '.')   # Time, when night begins.
 SUNRISE = get_time('T' + '05:00:00' + '.')  # Time, when night ends.
-THRESHOLD = 0                               # How much the value night_comments/all_comments must be so user could get
+THRESHOLD = 0.5                             # How much the value night_comments/all_comments must be so user could get
                                             # into our list.
 COMMENTS_FILE_NAME = 'Comments.xml'         # Name of file with comment data.
 USERS_FILE_NAME = 'Users.xml'               # Name of file with user data.
+RESULTS_FILE_NAME = 'results.htm'           # Name of file in which results will be writtens.
 
 
 def compare(a, b):
@@ -32,14 +33,25 @@ def nighttime(t):
 
 class User:
     def __init__(self, xml_element):
-        self.id = int(xml_element.attrib['Id'])
+        self.identifier = int(xml_element.attrib['Id'])
         self.reputation = int(xml_element.attrib['Reputation'])
         self.creation_date = xml_element.attrib['CreationDate']
+        self.creation_date = self.creation_date[0:self.creation_date.find('T')].replace('-', '.')
         self.display_name = xml_element.attrib['DisplayName']
         self.views = int(xml_element.attrib['Views'])
 
     def __repr__(self):
-        return repr((self.id, self.display_name))
+        return repr((self.identifier, self.display_name))
+
+    # Generates HTML code of row, which will contain information about this user.
+    def get_row(self, num, rev):
+        return '<tr><td>' + str(num) + '</td>' \
+               '<td><a href = "https://apple.stackexchange.com/users/' + str(self.identifier) + '">' \
+               '' + self.display_name + '</a></td>' \
+               '<td>' + self.creation_date + '</td>' \
+               '<td>' + str(self.reputation) + '</td>' \
+               '<td>' + str(self.views) + '</td>' \
+               '<td>' + str(round(rev * 100, 1)) + '%</td></tr>'
 
 
 root = ElementTree.parse(COMMENTS_FILE_NAME).getroot()
@@ -64,12 +76,25 @@ for user in comments_by_user.keys():
                 night_comments += 1
         if night_comments / len(comments_by_user[user]) >= THRESHOLD:
             user_ids.append((user, night_comments / len(comments_by_user[user])))
-user_ids = sorted(user_ids, key=lambda x: x[1])     # Sorting by relevance, just for fun.
+user_ids = sorted(user_ids, key=lambda x: -x[1])    # Sorting by relevance.
 
 root = ElementTree.parse(USERS_FILE_NAME).getroot()
 
 users_by_id = dict()                                # Key - ID of user, value - User object.
 for user in root.iter('row'):
     users_by_id[int(user.attrib['Id'])] = User(user)
-for identifier in user_ids:                         # Printing our list, just for fun.
-    print(users_by_id[identifier[0]], identifier[1])
+
+rows = ''                                           # Generating HTML.
+for i in range(0, len(user_ids)):
+    rows += users_by_id[user_ids[i][0]].get_row(i + 1, user_ids[i][1])
+
+header = '<!DOCTYPE style PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">' \
+         '<html><head><title>Results</title><link rel="stylesheet" href="style.css"></head><body><table><thead><tr>' \
+         '<td>No.</td><td>Username</td><td>Date</td><td>Reputation</td><td>Views</td><td>Relevance</td></tr>' \
+         '</thead><tbody>'
+
+footer = '</tbody></table></body></html>'
+
+file = open(RESULTS_FILE_NAME, 'w')                 # Writing the results.
+file.write(header + rows + footer)
+file.close()
